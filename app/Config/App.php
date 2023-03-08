b5b7e4a3857cb19f90c1266544d494809203cfb9
Rohan <rohankollambalath@gmail.com>
2023-03-08 05:31:59 +0000

    set up configs

--- app/Config/App.php
+++ app/Config/App.php
@@ -17,7 +17,7 @@ class App extends BaseConfig
      *
      *    http://example.com/
      */
-    public string $baseURL = 'http://localhost:8080/';
+    public string $baseURL = 'https://infs3202-187cdf27.uqcloud.net/project/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
